02effa53bb7560dfe0a7311dde4cc8eb7d911734
entcheva <christina.entcheva@gmail.com>
2017-01-19 14:40:07 -0500

    Build out AJAX call

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 $(document).ready(function() {
 
-  const URL = '';
+  const URL = 'http://www.strudel.org.uk/lookUP/json/?name=';
 
   // get the user query
-  // parse the user query
-  // format the url
-  // interpolate query with base url
-  // submit AJAX request
+  $('form#search-form').submit(function(event) {
+    event.preventDefault()
+    let $input = $('input#query')
+    let value = $input.val()
+    // parse the user query
+    let parsedInput = value.replace(/\s/g,'+')
+    // interpolate query with base url
+    let query = URL + parsedInput + '&callback=lk'
+    // submit AJAX request
+    $.ajax({
+      url: query,
+      dataType: 'jsonp',
+      success: function(data) {
+      debugger
+
+      }
+
+
+
+
+    }) // end of ajax
+
+
+
+
+
+
+  })
+
+
+
+
+
+
+
   // display the JSON data
   // display img
 
-  
+
 
 
 
