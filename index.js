abc5a3b7ce6218a0ded5ec513e62505282e6a68e
entcheva <christina.entcheva@gmail.com>
2017-01-19 13:58:08 -0500

    Add index html and js

---
+++ index.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  const URL = '';
+
+  // get the user query
+  // parse the user query
+  // format the url
+  // interpolate query with base url
+  // submit AJAX request
+  // display the JSON data
+  // display img
+
+  
+
+
+
+
+
+})
